5f90ecccba6ef876474ba3df7c388d882295086d
Константин <toshnotik666@mail.ru>
2021-12-24 15:56:01 +0300

    С5.6 Итоговый проект

--- app.py
+++ app.py
@@ -13,12 +13,15 @@ keys = {
     'рубль': 'RUB',
 }
 
+class ConvertionExeption(Exception):
+    pass
+
 
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-    text = 'Чтобы узнать стоимость, введите команду в следующей форме:\n <имя валюты> ' \
+    text = 'Чтобы узнать стоимость, введите команду в следующей форме:\n <коллическто переводимой валюты> <имя валюты> ' \
            '<в какую валюту перевести> ' \
-           '<коллическто переводимой валюты>\n Увидеть список доступных валют: /values'
+           '\n Увидеть список доступных валют: /values'
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['values'])
@@ -30,8 +33,32 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    quote, base, amount = message.text.split(' ')
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
+    values = message.text.split(' ')
+
+    if len(values) != 3:
+        raise ConvertionExeption('Запрос введен не корректно.')
+
+    amount, quote, base = values
+
+    if quote == base:
+        raise ConvertionExeption(f'Вы ввели одинаковую валюту.')
+
+    try:
+        quote_tiker = keys[quote]
+    except KeyError:
+        raise ConvertionExeption(f'Не удалось обработать валюту {quote}')
+
+    try:
+        base_tiker = keys[base]
+    except KeyError:
+        raise ConvertionExeption(f'Не удалось обработать валюту {base}')
+
+    try:
+        amount = float(amount)
+    except ValueError:
+        raise ConvertionExeption(f'Не удалось обработать колличество {amount}')
+
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_tiker}&tsyms={base_tiker}')
     total_base = json.loads(r.content)[keys[base]]
     total = total_base * float(amount)
     text = f'Цена {amount} {quote} в {base} - {total}'
